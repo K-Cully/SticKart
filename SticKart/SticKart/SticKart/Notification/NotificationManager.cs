771d5abca053a386053c1c1fe44f029bf213b1e4
K-Cully <kcul.dev@gmail.com>
2013-01-14 11:06:44 +0000

    Created a thread safe notification manager

---
+++ SticKart/SticKart/SticKart/Notification/NotificationManager.cs
@@ -0,0 +1,93 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NotificationManager.cs" company="None">
+// Copyright Keith Cully 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SticKart.Notification
+{
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// Manages the update and display of notifications.
+    /// </summary>
+    public sealed class NotificationManager
+    {
+        /// <summary>
+        /// The notification manager singleton.
+        /// </summary>
+        private static volatile NotificationManager manager = null;
+
+        /// <summary>
+        /// An object to lock on which ensures thread-safe instanciation of the notification manager.
+        /// </summary>
+        private static object mutex = new object();
+        
+        /// <summary>
+        /// A queue of notifications to display.
+        /// </summary>
+        private volatile Queue<Notification> notificationQueue;
+
+        /// <summary>
+        /// Prevents initialization of the <see cref="NotificationManager"/> class.
+        /// </summary>
+        private NotificationManager()
+        {
+            this.notificationQueue = new Queue<Notification>();
+        }
+
+        /// <summary>
+        /// Gets the notification manager singleton.
+        /// </summary>
+        public static NotificationManager Instance
+        {
+            get
+            {
+                if (NotificationManager.manager == null)
+                {
+                    lock (NotificationManager.mutex)
+                    {
+                        NotificationManager.manager = new NotificationManager();
+                    }
+                }
+
+                return NotificationManager.manager;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether there are any notifications active or not.
+        /// </summary>
+        public bool NotificationsActive
+        {
+            get
+            {
+                lock (NotificationManager.mutex)
+                {
+                    return this.notificationQueue.Count > 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the currently displayed notification, if any.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
+        /// <param name="closeNotification">A value indicating whether to force close the currently displayed notification or not.</param>
+        public void Update(GameTime gameTime, bool closeNotification)
+        {
+            lock (NotificationManager.mutex)
+            {
+                if (this.notificationQueue.Count > 0)
+                {
+                    this.notificationQueue.Peek().Update(gameTime, closeNotification);
+                    if (!this.notificationQueue.Peek().Active)
+                    {
+                        this.notificationQueue.Dequeue();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SticKart/SticKart/SticKart/Notification/NotificationType.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NotificationType.cs" company="None">
+// Copyright Keith Cully 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SticKart.Notification
+{
+    /// <summary>
+    /// An enumeration of notification types.
+    /// </summary>
+    public enum NotificationType
+    {
+        /// <summary>
+        /// No notification.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// A notification on how to perform a push gesture.
+        /// </summary>
+        PushGesture,
+
+        /// <summary>
+        /// A notification on how to perform a swipe gesture.
+        /// </summary>
+        SwipeGesture,
+
+        /// <summary>
+        /// A notification on how to perform a voice command.
+        /// </summary>
+        VoiceCommand,
+
+        /// <summary>
+        /// A notification on how to control the stick man's run action.
+        /// </summary>
+        Run,
+
+        /// <summary>
+        /// A notification on how to control the stick man's jump action.
+        /// </summary>
+        JumpUp,
+
+        /// <summary>
+        /// A notification on how to control the stick man's jump down action.
+        /// </summary>
+        JumpDown,
+
+        /// <summary>
+        /// A notification on how to control the stick man's crouch action.
+        /// </summary>
+        Crouch,
+        
+        /// <summary>
+        /// A notification about the scrolling death that awaits.
+        /// </summary>
+        ScrollingDeath,
+
+        /// <summary>
+        /// A notification about how to exit a level.
+        /// </summary>
+        Exit,
+
+        /// <summary>
+        /// A notification about how to use the cart.
+        /// </summary>
+        Cart,
+
+        /// <summary>
+        /// A notification about bonuses.
+        /// </summary>
+        Bonus,
+
+        /// <summary>
+        /// A notification about obstacles.
+        /// </summary>
+        Obstacle,
+
+        /// <summary>
+        /// A notification about power ups.
+        /// </summary>
+        PowerUp
+    }
+}
