ff8808ee334b8604dd7604410e2041d5d4c51dca
K-Cully <kcul.dev@gmail.com>
2013-02-26 14:10:11 +0000

    Added a reference to the data service and started writing the connection manager KEIT-155

---
+++ SticKart/SticKart/SticKart/Game/AzureServices/ScoreServiceManager.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ScoreServiceManager.cs" company="None">
+// Copyright Keith Cully 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SticKart.Game.AzureServices
+{
+    using System;
+    using System.Data.Services.Client;
+    using SticKartScores;
+
+    /// <summary>
+    /// Manages connection to and querying of the cloud hosted high score service.
+    /// </summary>
+    public class ScoreServiceManager
+    {
+        /// <summary>
+        /// The context to use when querying the score service.
+        /// </summary>
+        private SticKartScores_0Entities context;
+
+        /// <summary>
+        /// The uri of the hosted data service.
+        /// </summary>
+        private const Uri serviceUri = new Uri("http://stickarthighscores.cloudapp.net/ScoresWcfDataService.svc");
+
+        /// <summary>
+        /// Prevents the initialization of the <see cref="ScoreServiceManager"/> class.
+        /// </summary>
+        private ScoreServiceManager()
+        {
+        }
+    }
+}
