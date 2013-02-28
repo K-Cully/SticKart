a78127eeadba832fc60db68cc98797c81f748d33
K-Cully <kcul.dev@gmail.com>
2013-02-28 13:29:18 +0000

    Added high score types KEIT-153

---
+++ SticKart/SticKart/SticKart/Game/Level/HighScoreType.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HighScoreType.cs" company="None">
+// Copyright Keith Cully 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SticKart.Game.Level
+{
+    /// <summary>
+    /// An enumeration of high score types.
+    /// </summary>
+    public enum HighScoreType
+    {
+        /// <summary>
+        /// Specifys a global high score type.
+        /// </summary>
+        Global,
+
+        /// <summary>
+        /// Specifys a local high score type.
+        /// </summary>
+        Local,
+
+        /// <summary>
+        /// Specifys no high score.
+        /// </summary>
+        None
+    }
+}
