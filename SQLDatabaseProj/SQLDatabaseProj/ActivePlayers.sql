536150fb3047079599bf67ade7ffbf34f471533c
K-Cully <kcul.dev@gmail.com>
2013-03-16 08:54:21 +0000

    (Networking) Added matchmaking and statistic tables

---
+++ SQLDatabaseProj/SQLDatabaseProj/ActivePlayers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ActivePlayers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Ip] NCHAR(39) NOT NULL,
+    [Port] NCHAR(4) NOT NULL,
+    [State] NCHAR(1) NOT NULL, 
+    [Player] INT NOT NULL, 
+    CONSTRAINT [FK_ActivePlayers_Statistics] FOREIGN KEY ([Player]) REFERENCES [Statistics]([Id])
+)
---
+++ SQLDatabaseProj/SQLDatabaseProj/Statistics.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Statistics]
+(
+    [Id] INT NOT NULL IDENTITY,
+    [Name] NCHAR(32) NOT NULL,
+    [Password] NCHAR(4) NOT NULL,
+    [GamesPlayed] INT NOT NULL DEFAULT 0,
+    [GamesWon] INT NOT NULL DEFAULT 0,
+    [GamesLost] INT NOT NULL DEFAULT 0,
+    PRIMARY KEY ([Id]) 
+)
